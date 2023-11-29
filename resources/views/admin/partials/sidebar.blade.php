33b5c4f14f8aeea76b4213f9ff2b1d747d3cc8e6
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-11-29 17:29:42 +0100

    add header and sidebar

---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,12 @@
+<aside class="bg-dark">
+    <nav class="my-3">
+        <ul class=" p-0">
+            <li class="text-center my-2">
+                <a class="btn w-75 m-auto" href="{{route('admin.home')}}"> Dashboard</a>
+            </li>
+            <li class="text-center my-2">
+                <a class="btn w-75 m-auto" href="#"> Lista Post</a>
+            </li>
+        </ul>
+    </nav>
+</aside>
