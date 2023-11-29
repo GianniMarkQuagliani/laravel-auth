ae9d223a632a2cdad4652cc29829d4b1fabd2b6e
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-11-29 17:50:47 +0100

    add route on web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Guest\PageController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\TagController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,6 +32,9 @@ Route::middleware(['auth', 'verified'])
 ->name('admin.')
 ->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('home');
+    Route::resource('posts', PostController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('tags', TagController::class);
 });
 
 require __DIR__.'/auth.php';
