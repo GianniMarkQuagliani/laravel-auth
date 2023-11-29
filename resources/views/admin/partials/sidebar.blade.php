1ff6f7fbed6c89c643ed428ed352afc66cd8f588
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-11-29 17:38:23 +0100

    update css

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,11 +1,11 @@
 <aside class="bg-dark">
-    <nav class="my-3">
-        <ul class=" p-0">
-            <li class="text-center my-2">
-                <a class="btn w-75 m-auto" href="{{route('admin.home')}}"> Dashboard</a>
+    <nav>
+        <ul>
+            <li>
+                <a href="{{route('admin.home')}}"> Dashboard</a>
             </li>
-            <li class="text-center my-2">
-                <a class="btn w-75 m-auto" href="#"> Lista Post</a>
+            <li >
+                <a  href="#"> Lista Post</a>
             </li>
         </ul>
     </nav>
